cde3baa22e47ac2dc5f9b2e3269cb59469a38c44
Tatsuro <contacts@tatsuro.dev>
2021-01-06 06:57:12 +0900

    createGlobalStyleでglobal css作成 6

--- styled-components-v5/src/components/App.js
+++ styled-components-v5/src/components/App.js
@@ -1,8 +1,20 @@
 import { Button } from 'components/common'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+`
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>App</h1>
       <Button>Test</Button>
     </>
