c5c9b2fa0977bfee53681e351f9c3ff90bdc3f00
Tatsuro <contacts@tatsuro.dev>
2021-01-07 08:03:01 +0900

    styled-componentsでmedia query使用 14

--- styled-components-v5/src/components/common/Header.js
+++ styled-components-v5/src/components/common/Header.js
@@ -10,19 +10,32 @@ const HeaderWrapper = styled.header`
   padding: 0 16px;
   position: fixed;
   top: 0;
-  background: #eee;
+  background-image: linear-gradient(to right, #f8049c, #fdd54f);
+  border-bottom: 3px solid #fdd54f;
 `
 
 const Menu = styled.nav`
-  display: flex;
-  position: relative;
-  width: initial;
-  border-bottom: none;
-  margin: auto 0 auto auto;
+  display: block;
   font-family: 'Open Sans';
-  background: none;
-  left: initial;
-  top: initial;
+  position: absolute;
+  width: 100%;
+  top: 60px;
+  left: 0;
+  padding: 8px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #fdd54f;
+  background: white;
+
+  @media (min-width: 768px) {
+    display: flex;
+    background: none;
+    left: initial;
+    top: initial;
+    margin: auto 0 auto auto;
+    border-bottom: none;
+    position: relative;
+    width: initial;
+  }
 `
 
 const Link = ({ isActive, children, ...props }) => {
@@ -37,6 +50,7 @@ const StyledLink = styled(Link)`
   margin: auto 0;
   /* react-router-domのLink componentは、anchor tagに変換されるがisActive propertyがないので、直接isActiveを渡すとerrorが発生する。一度別名のReactRouterDomLinkのpropsとして受け取り、その中で間接的にspread operatorでattributeを渡すとanchor tagのattributeとして使用できるようになる */
   font-weight: ${(p) => (p.isActive ? 'bold' : 'normal')};
+  color: black;
 `
 
 export function Header() {
