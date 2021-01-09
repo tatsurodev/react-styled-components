81410c936b0144d1c8bd52131c67d97deb10c637
Tatsuro <contacts@tatsuro.dev>
2021-01-10 02:15:44 +0900

    passwordのshow, hide toggle button作成 18

--- styled-components-v5/src/components/common/PasswordInput.js
+++ styled-components-v5/src/components/common/PasswordInput.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Input } from './Input'
 
+const PasswordInputWrapper = styled.div`
+  display: flex;
+  /* ~で隣接selector */
+  ~ div {
+    margin-bottom: 8px;
+  }
+`
+
 // attrs helperはdefault propsやpropsによって動的なcssを実装する際に便利
 const PasswordInputStyled = styled(Input).attrs(() => ({
   type: 'password',
@@ -11,8 +19,35 @@ const PasswordInputStyled = styled(Input).attrs(() => ({
   border-bottom-right-radius: 0;
 `
 
-export function PasswordInput(props) {
-  const [showPassword, setShowPassword] = useState(false)
+const ToggleButton = styled.div`
+  height: 40px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  font-size: 0.9em;
+  display: flex;
+  padding: 8px;
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background: white;
+  font-weight: bold;
+  cursor: pointer;
+  user-select: none;
+  color: black;
+`
 
-  return <PasswordInputStyled {...props} />
+export function PasswordInput(props) {
+  const [showPassword, setShowPassword] = useState(true)
+
+  return (
+    <>
+      <PasswordInputWrapper>
+        <PasswordInputStyled {...props} />
+        <ToggleButton onClick={() => setShowPassword((s) => !s)}>
+          {showPassword ? 'Hide' : 'Show'}
+        </ToggleButton>
+      </PasswordInputWrapper>
+      <div>{showPassword ? props.value : ''}</div>
+    </>
+  )
 }
